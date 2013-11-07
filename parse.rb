edd3076b260a3e73276ea3310d1ebdda755c8663
Dan S Turner <turner@nowwithmoredan.com>
2013-11-07 12:44:37 -0800

    Add output with formatting of CSV in terminal

--- parse.rb
+++ parse.rb
@@ -4,6 +4,8 @@ file = File.read('data.csv')
 data = CSV.parse(file)
 
 clean = data.slice((5..58))
+clean = clean.transpose.slice((0..5))
+clean = clean.transpose
 
 # puts "HEADERS!"
 
@@ -22,6 +24,14 @@ columns_max_length = clean.transpose.map {    # now we've got columns!
     .reject { |cell| cell.nil? }              # removes empty cells
     .map { |cell| cell.length }               # finds cell lengths for math
     .max                                      # retuns the highest number length
-                                          }
+  }
 
-puts columns_max_length
+
+clean.each { |row|
+  row.each_index { |index|
+    column_width = columns_max_length[index]
+    word_length = row[index].length
+    print "|  #{row[index]}#{' ' * (column_width - word_length)} "
+  }
+  print " |\n"
+}
